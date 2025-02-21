aa7214b6f2aaa0b5be1434ab3da3a4b73ad0fd3a
Atchuta Vani <vani.c005@gmail.com>
2025-02-21 15:38:50 -0800

    Create LC53_MergeIntervals.java

---
+++ LC53_MergeIntervals.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class MergeIntervals {
+    public static void main(String[] args) {
+
+        int[][] intervals = {{1,4},{3,8},{9,10}};
+        print(mergeIntervals(intervals));
+    }
+    public static void print(int[][] result){
+        System.out.print("[");
+        for(int i = 0; i < result.length; i++) {
+            for(int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+
+        }
+        System.out.print("]");
+    }
+    public static int[][] mergeIntervals(int[][] intervals) {
+
+            List<int[]> list = new ArrayList<int[]>();
+            if(intervals.length == 0) return new int[][]{};
+            list.add(intervals[0]);
+            for (int i = 1; i < intervals.length; i++) {
+                int[] interval = intervals[i];
+                int[] result = list.get(list.size()-1);
+                int left = result[0];
+                int right = result[1];
+                if(right > interval[0]) {//merge interval
+                    right = Math.max(right, interval[1]);
+                    list.remove(list.size() - 1);
+                    list.add(new int[]{left,right});
+                }if(right < interval[0]) {
+                    //add new interval
+                    list.add(intervals[i]);
+                }
+            }
+            return list.toArray(new int[list.size()][]);
+
+        }
+    }
+
+
