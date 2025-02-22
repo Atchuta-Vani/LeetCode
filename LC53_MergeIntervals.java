0d853909b7a37849332579877ea4fa47c48f2971
Atchuta Vani <vani.c005@gmail.com>
2025-02-21 16:24:18 -0800

    Update LC53_MergeIntervals.java

--- LC53_MergeIntervals.java
+++ LC53_MergeIntervals.java
@@ -21,16 +21,21 @@ class MergeIntervals {
 
             List<int[]> list = new ArrayList<int[]>();
             if(intervals.length == 0) return new int[][]{};
+            Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
             list.add(intervals[0]);
             for (int i = 1; i < intervals.length; i++) {
                 int[] interval = intervals[i];
+                //System.out.println("Current Left and Right are "+interval[0]+" "+interval[1]);
+
                 int[] result = list.get(list.size()-1);
                 int left = result[0];
                 int right = result[1];
-                if(right > interval[0]) {//merge interval
+                //System.out.println("Left and Right are "+left+" "+right);
+                if(right >= interval[0]) {//merge interval
                     right = Math.max(right, interval[1]);
                     list.remove(list.size() - 1);
                     list.add(new int[]{left,right});
+                    //System.out.println(right);
                 }if(right < interval[0]) {
                     //add new interval
                     list.add(intervals[i]);
