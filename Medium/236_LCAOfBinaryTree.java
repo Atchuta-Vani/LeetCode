52278b3141360a5a512e00ef5c90fcf3ffd9d06a
Atchuta Vani <vani.c005@gmail.com>
2025-03-14 20:12:24 -0700

    Create 236_LCAOfBinaryTree.java

---
+++ Medium/236_LCAOfBinaryTree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ // 236 LCA of Binary Tree.
+ //Time Complexity: O(N)
+ /**
+ Algorithm
+Base Case:
+
+If the current node is null, return null.
+If the current node is either p or q, return the current node.
+Recursive Traversal:
+
+Recursively find LCA in the left subtree.
+Recursively find LCA in the right subtree.
+Decision Making:
+
+If both left and right recursive calls return a non-null value, the current node is the Lowest Common Ancestor.
+If only one side returns a non-null value, return that side.
+If both are null, return null.
+ 
+  */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if(root == null || root.val == p.val || root.val == q.val ){
+            return root;
+        }
+
+        TreeNode  left = lowestCommonAncestor(root.left, p,q);
+        TreeNode  right = lowestCommonAncestor(root.right, p,q);
+
+        if(left != null && right != null){
+            return root;
+        }
+        
+        return (left !=null)? left:right;// what is this doing ?
+
+
+    }
+}
