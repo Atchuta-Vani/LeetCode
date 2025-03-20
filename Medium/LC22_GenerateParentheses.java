c5ba097108dcc25b40020c2a23802db63049418a
Atchuta Vani <vani.c005@gmail.com>
2025-03-20 13:29:33 -0700

    Create LC22_GenerateParentheses.java

---
+++ Medium/LC22_GenerateParentheses.java
@@ -0,0 +1,24 @@
+
+//22. Generate Parentheses
+//backtrack
+
+
+class Solution {
+    List<String> result = new ArrayList();
+
+    public List<String> generateParenthesis(int n) {
+        backtrack(0,0,"",n);
+        return result;
+    }
+    void backtrack(int open, int close, String curString,int n){
+        if(curString.length() == 2*n){
+            result.add(curString);
+        }
+        if(open<n){
+            backtrack(open+1,close,curString+"(",n);
+        }
+        if(close<open){
+            backtrack(open,close+1,curString+")",n);
+        }
+    }
+}
