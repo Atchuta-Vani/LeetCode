84019a37e1a4b679d8d12f671b1ed7ec7bb0688d
Atchuta Vani <vani.c005@gmail.com>
2025-04-12 13:53:19 -0700

    Update LC28_FindTheIndexOfFirstOccurance.java

--- LC28_FindTheIndexOfFirstIndex.java
+++ LC28_FindTheIndexOfFirstIndex.java
@@ -1,8 +1,14 @@
+//Sliding window.
+//28 Find the Index of the First Occurrence in a String
 class Solution {
     public int strStr(String haystack, String needle) {
         int left = 0;
         int n = needle.length();
         int h =  haystack.length();
+        if(n == 0){
+            return 0;
+        }
+
         for(int right = n-1; right<h; right++){
             String str = haystack.substring(left,right+1); 
             //System.out.println("substring"+ str);           
