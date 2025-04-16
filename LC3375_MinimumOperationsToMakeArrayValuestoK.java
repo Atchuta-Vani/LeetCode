dd5789e1f1821dd3a798f5c5d3938160a91a2c89
Atchuta Vani <vani.c005@gmail.com>
2025-04-15 18:04:02 -0700

    Update LC3375_MinimumOperationsToMakeArrayValuestoK.java

--- LC3375_MinimumOperationsToMakeArrayValuestoK.java
+++ LC3375_MinimumOperationsToMakeArrayValuestoK.java
@@ -1,6 +1,7 @@
 /**
 Hint: Number of distinct integers greater than k is the solution. 
 
+Find the largest and replace with second largest. 
 if the number is less than k, return -1. 
 if the number is greater than k, keep adding it to set and return size of set at the end. 
  */
