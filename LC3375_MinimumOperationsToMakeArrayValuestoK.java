c7dcec18cd1bce46d19cf3f87b234066d0d8860c
Atchuta Vani <vani.c005@gmail.com>
2025-04-15 18:02:47 -0700

    Create LC3375_MinimumOperationsToMakeArrayValuestoK.java

---
+++ LC3375_MinimumOperationsToMakeArrayValuestoK.java
@@ -0,0 +1,19 @@
+/**
+Hint: Number of distinct integers greater than k is the solution. 
+
+if the number is less than k, return -1. 
+if the number is greater than k, keep adding it to set and return size of set at the end. 
+ */
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        Set<Integer> set = new HashSet();
+        for(int num:nums){
+            if(num>k){
+                set.add(num);
+            }
+            if(num<k)
+                return -1;
+        }
+        return set.size();
+    }
+}
