116984b7151c9921c1ee3a25555cf15b9ca0ba75
Atchuta Vani <vani.c005@gmail.com>
2025-04-18 16:25:15 -0700

    Update LC121_BestTime2SellStock.java

--- LC121_BestTime2SellStock.java
+++ LC121_BestTime2SellStock.java
@@ -1,17 +1,23 @@
-//121. Best Time to Buy and Sell Stock
+/**
+Buy low, sell high.
+One Pass (Greedy)
+Keep track of the lowest price as you go.
+At each step, calculate potential profit = current price - min_price.
+Update the max profit if this one is higher.
+
+ */
+
 class Solution {
     public int maxProfit(int[] prices) {
-
-        int minPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
-        for(int n :prices){
-            if(n < minPrice){
-                minPrice = n;
-            }
-            if(maxProfit < n-minPrice){
-                maxProfit = n-minPrice;
-            }
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        if(prices == null || prices.length == 1)
+            return 0;
+        for(int i = 0;i<prices.length;i++){
+            min = Math.min(min,prices[i]);
+            max = Math.max(max,prices[i]-min);            
         }
-        return maxProfit;
+        
+        return max;
     }
 }
